ff1ab4682579bac6a4e914fcb5de170dcaedc43a
rusti <mrgregory1@gmail.com>
2020-08-20 10:42:29 -0400

    work in progress

--- check_positives.py
+++ check_positives.py
@@ -19,10 +19,70 @@ print("\n")
 print("checking new daily covid form entries")
 print("\n")
 
-# setup recipients emails by scchool
+
+def get_school_lists():
+
+    # open up google sheet to see if new staff have been added
+    gc = pygsheets.authorize(outh_file="client_secret.json")
+    school_covid_lists = gc.open_by_key("1ZIisaL_CQkbblkh7Furrd9J_MIQMeViWLrewgJndIns")
+    swanton_list = school_covid_lists.worksheet_by_title("Swanton")
+
+    # download all data from sheet as cell_matrix
+    cell_matrix = swanton_list.get_all_values(returnas="matrix")
+    # print(cell_matrix)
+
+    # gather 'keys' for new dict from 1st row in sheet
+    dict_key_list = [x for x in cell_matrix[0]]
+    # pprint(dict_key_list)
+    dict_key_list = [
+        "First Name",
+        "Last Name",
+        "Work Email",
+        "Ivisions ID",
+    ]
+
+    # initialize dict for data
+    swanton_checklist = []
+
+    # put cell_matrix list of lists into a list of dictionaries
+    for count, row in enumerate(cell_matrix):
+        if row[0] != "":
+            line_dict = dict(zip(dict_key_list, row))
+            swanton_checklist.append(line_dict)
+
+    # print(swanton_checklist)
+    print("Swanton list complete")
+
+    return swanton_checklist
+
+
+# swanton_checklist = get_school_lists()
+
+# # setup recipients emails by scchool
+# recipients_dict = {
+#     "Central Office": "russell.gregory@mvsdschools.org",
+#     "Swanton Elementary: Central Building": "russell.gregory@mvsdschools.org",
+#     "MVU": "russell.gregory@mvsdschools.org",
+#     "MVU-Connect": "russell.gregory@mvsdschools.org",
+#     "Franklin Central School": "russell.gregory@mvsdschools.org",
+#     "Highgate Elementary": "russell.gregory@mvsdschools.org",
+#     "Swanton Elementary: Babcock Building": [
+#         "russell.gregory@mvsdschools.org",
+#         "robert.gervais@mvsdschools.org",
+#     ],
+# }
+
 recipients_dict = {
-    "Central Office": "russell.gregory@mvsdschools.org",
-    "Swanton Elementary: Central Building": "russell.gregory@mvsdschools.org",
+    "Central Office": "Pierrette.Bouchard@mvsdschools.org",
+    "Swanton Elementary: Central Building": [
+        "Danielle.Loiselle@mvsdschools.org",
+        "russell.gregory@mvsdschools.org",
+    ],
+    "MVU": ["Alissa.Graves@mvsdschools.org", "Lynn.Billado@mvsdschools.org"],
+    "MVU-Connect": "russell.gregory@mvsdschools.org",
+    "Franklin Central School": "alita.boomhower@mvsdschools.org",
+    "Highgate Elementary": "Jennifer.Gagne@mvsdschools.org",
+    "Swanton Elementary: Babcock Building": "Wendy.Culligan@mvsdschools.org",
 }
 
 
@@ -50,6 +110,7 @@ def check_for_new_staff():
         "Symptoms",
         "Exposure",
         "Household",
+        "ID",
     ]
 
     # initialize dict for data
@@ -58,7 +119,7 @@ def check_for_new_staff():
     # put cell_matrix list of lists into a list of dictionaries
     for count, row in enumerate(cell_matrix):
         if row[0] != "":
-            if row[8] == "":
+            if row[9] == "":
                 line_dict = dict(zip(dict_key_list, row))
                 # add count so I can add x to appropriate row later
                 line_dict["row_number"] = count + 1
@@ -89,6 +150,7 @@ def email_nurse(staff, recipients_dict):
 
                 Name: {staff['Name']}
                 Buildings Working in Today: {staff['Buildings']}
+                Form Submitted: {staff['Timestamp']}
                 """
                 html = f"""<table style="border-collapse: collapse;">
                         <tbody>
@@ -136,17 +198,17 @@ if len(worksheet_data) == 0:
 
 else:
     # list contains items
-    print("New response found")
+    print("New response found\n")
     is_new_response = True
 
     for staff in worksheet_data:
         if "YES" in staff.values():
-            print("yes")
+            print(f"Sending mail for {staff['Name']}\n")
             email_nurse(staff, recipients_dict)
             # mark new staff member as processed with X in column I
 
-        mark_as_finished_cell = "I" + str(staff["row_number"])
-        initial_form_sheet.update_value(mark_as_finished_cell, "X")
+        mark_as_finished_cell = "J" + str(staff["row_number"])
+        initial_form_sheet.update_value(mark_as_finished_cell, "Q")
 
 
 print("\n\nfinished")
